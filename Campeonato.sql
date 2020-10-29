f8450dbe33e537e5a7bc62e40deb6cea516ba6d3
Adriel Rosanelli <62855784+adrielrosanelli@users.noreply.github.com>
2020-10-29 20:34:16 -0300

    Update Campeonato.sql

--- Campeonato.sql
+++ Campeonato.sql
@@ -36,9 +36,48 @@ quantidade_cartao_vermelho int not null,
 quantidade_cartao_amarelo int not null
 )
 Questão 1
-select tb_jogador.id,tb_jogador.nome,tb_jogador.numero_camisa, tb_time.nome, tb_time.id from tb_jogador inner join tb_time on tb_jogador.codigo_time = tb_time.id
 
+SELECT tb_jogador.nome jogador, tb_time.nome time 
+from tb_partida 
+inner join tb_time on tb_partida.codigo_mandante=tb_time.id or tb_partida.codigo_visitante=tb_time.id 
+inner join tb_jogador on tb_time.id=tb_jogador.codigo_time 
+group by time, jogador
 
 
 Questão 2
-select tb_partida.id,tb_detalhamentopartida.quantidade_gol,tb_detalhamentopartida.quantidade_cartao_vermelho,tb_detalhamentopartida.quantidade_cartao_amarelo,tb_jogador.nome from tb_time inner join tb_jogador on tb_time.id = tb_jogador.codigo_time inner join tb_partida on codigo_mandante = tb_time.id or codigo_visitante = tb_time.id inner join tb_detalhamentopartida on tb_detalhamentopartida.codigo_jogador = tb_jogador.id and tb_detalhamentopartida.codigo_
+
+SELECT tb_time.nome time, tb_partida.data data_partida, 
+SUM(tb_detalhamentopartida.quantidade_gol) gol, 
+SUM(tb_detalhamentopartida.quantidade_cartao_amarelo) cartão_amarelo, 
+SUM(tb_detalhamentopartida.quantidade_cartao_vermelho) cartão_vermelho 
+from tb_detalhamentopartida inner join tb_partida on tb_partida.id=tb_detalhamentopartida.codigo_partida 
+inner join tb_campeonato on tb_campeonato.id=tb_partida.codigo_campeonato 
+inner join tb_jogador on tb_jogador.id=tb_detalhamentopartida.codigo_jogador 
+inner join tb_time on tb_time.id=tb_jogador.codigo_time 
+GROUP BY data_partida, time
+
+Questão 3
+
+SELECT tb_time.nome time, tb_campeonato.nome campeonato, 
+SUM(tb_detalhamentopartida.quantidade_gol) gol, 
+SUM(tb_detalhamentopartida.quantidade_cartao_amarelo) cartão_amarelo, 
+SUM(tb_detalhamentopartida.quantidade_cartao_vermelho) cartão_vermelho 
+FROM tb_detalhamentopartida 
+INNER JOIN tb_partida ON tb_partida.id=tb_detalhamentopartida.codigo_partida inner join tb_campeonato on tb_campeonato.id=tb_partida.codigo_campeonato 
+INNER JOIN tb_jogador ON tb_jogador.id=tb_detalhamentopartida.codigo_jogador inner join tb_time on tb_time.id=tb_jogador.codigo_time 
+GROUP BY campeonato, time
+
+Questão 4
+
+
+SELECT tb_campeonato.nome campeonato, tb_time.nome time, 
+SUM(CASE
+	WHEN tb_partida.codigo_ganhador=tb_time.id THEN 3
+    WHEN tb_partida.codigo_ganhador is NULL THEN 1
+    ELSE 0
+END) AS pontos
+from tb_partida 
+inner join tb_time on tb_partida.codigo_mandante=tb_time.id or tb_partida.codigo_visitante=tb_time.id 
+inner join tb_campeonato on tb_campeonato.id=tb_partida.codigo_campeonato
+GROUP BY campeonato, time
+ORDER BY pontos DESC
